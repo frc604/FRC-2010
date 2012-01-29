58abd70e5d4e46b16d64b0b6af5626d9b1c750aa
Michael Smith <mdsmtp@gmail.com>
2012-01-29 14:01:34 -0800

    Code update!

--- Robot2010Overkill.cpp
+++ Robot2010Overkill.cpp
@@ -34,6 +34,13 @@
 		#define DRIVER_SHIFT_BUTTON 1
 		#define DRIVER_POKEY_BUTTON 3
 
+/* Xbox button configuration. */
+	#define XBOX_KICK_BUTTON 1
+	#define XBOX_BALL_MAGNET_IN_BUTTON 6
+	#define XBOX_BALL_MAGNET_OUT_BUTTON 5
+	
+	#define XBOX_POKEY_BUTTON 2
+
 /* Actuator polarity and speed configuration. */
 	#define SOLENOID_SHIFTER_HIGH_POWER_DIRECTION true
 	#define SOLENOID_SHIFTER_LOW_POWER_DIRECTION false
@@ -66,6 +73,10 @@ class Robot2010Overkill : public SimpleRobot {
 	DoubleSolenoid solenoidPokey;
 	DoubleSolenoid solenoidReload;
 	
+	float virtualThrottle;
+	bool trackingThrottle;
+	Timer *throttleTimer;
+	
 	public:
 		Robot2010Overkill(void):
 			joystickManipulator(MANIPULATOR_JOYSTICK_USB_PORT),
@@ -84,6 +95,147 @@ class Robot2010Overkill : public SimpleRobot {
 			driveTrain.SetSafetyEnabled(false);
 			
 			compressorPump = new Compressor(PRESSURE_SWITCH_PORT, COMPRESSOR_PORT);
+			throttleTimer = new Timer();
+			
+			virtualThrottle = 0;
+			trackingThrottle = false;
+		}
+		
+		typedef enum
+		{
+			kKick = 0,
+			kSuck = 1,
+			kSpit = 2,
+			kShift = 3,
+			kPokey = 4
+		} ActionType;
+		
+		typedef enum
+		{
+			kLeftX = 0,
+			kLeftY = 1,
+			kRightX = 2,
+			kRightY = 3,
+			kThrottle = 4
+		} AxisType;
+		
+		bool GetAction(ActionType action) {
+			bool isXbox = joystickDriveLeft.GetThrottle() == 0;
+			
+			switch(action) {
+				case kKick:
+					if(isXbox)
+						return joystickManipulator.GetRawButton(XBOX_KICK_BUTTON);
+					else
+						return joystickManipulator.GetRawButton(MANIPULATOR_KICK_BUTTON);
+					
+					break;
+				case kSuck:
+					if(isXbox)
+						return joystickManipulator.GetRawButton(XBOX_BALL_MAGNET_IN_BUTTON);
+					else
+						return joystickManipulator.GetRawButton(MANIPULATOR_BALL_MAGNET_IN_BUTTON);
+					
+					break;
+				case kSpit:
+					if(isXbox)
+						return joystickManipulator.GetRawButton(XBOX_BALL_MAGNET_OUT_BUTTON);
+					else
+						return joystickManipulator.GetRawButton(MANIPULATOR_BALL_MAGNET_OUT_BUTTON);
+					
+					break;
+				case kShift:
+					if(isXbox)
+						return joystickManipulator.GetRawAxis(3) > 0.2 || joystickManipulator.GetRawAxis(3) < -0.2;
+					else
+						return joystickDriveLeft.GetRawButton(DRIVER_SHIFT_BUTTON) || joystickDriveRight.GetRawButton(DRIVER_SHIFT_BUTTON);
+					
+					break;
+				case kPokey:
+					if(isXbox)
+						return joystickManipulator.GetRawButton(XBOX_POKEY_BUTTON);
+					else
+						return joystickDriveLeft.GetRawButton(DRIVER_POKEY_BUTTON) || joystickDriveRight.GetRawButton(DRIVER_POKEY_BUTTON);
+					
+					break;
+			}
+			
+			return false;
+		}
+		
+		float GetAxis(AxisType axis) {
+			bool isXbox = joystickDriveLeft.GetThrottle() == 0;
+			
+			switch(axis) {
+				case kLeftX:
+					if(isXbox)
+						return joystickManipulator.GetRawAxis(1);
+					else
+						return joystickDriveLeft.GetX();
+					
+					break;
+				case kLeftY:
+					if(isXbox)
+						return joystickManipulator.GetRawAxis(2);
+					else
+						return joystickDriveLeft.GetY();
+					
+					break;
+				case kRightX:
+					if(isXbox)
+						return joystickManipulator.GetRawAxis(4);
+					else
+						return joystickDriveRight.GetX();
+					
+					break;
+				case kRightY:
+					if(isXbox)
+						return joystickManipulator.GetRawAxis(5);
+					else
+						return joystickDriveRight.GetY();
+					
+					break;
+				case kThrottle:
+					if(isXbox) {
+						bool buttonX = joystickManipulator.GetRawButton(3);
+						bool buttonY = joystickManipulator.GetRawButton(4);
+						
+						if(buttonX || buttonY) {
+							if(!trackingThrottle) {
+								throttleTimer->Reset();
+								throttleTimer->Start();
+								
+								trackingThrottle = true;
+							} else {
+								virtualThrottle += throttleTimer->Get() / 3 * ((buttonX) ? -1 : 1);
+								throttleTimer->Reset();
+								
+								if(virtualThrottle > 1)
+									virtualThrottle = 1;
+								if(virtualThrottle < -1)
+									virtualThrottle = -1;
+							}
+						} else {
+							if(trackingThrottle) {
+								throttleTimer->Stop();
+								trackingThrottle = false;
+							}
+						}
+						
+						return virtualThrottle;
+					} else {
+						if(trackingThrottle) {
+							throttleTimer->Stop();
+							trackingThrottle = false;
+						}
+						
+						return joystickManipulator.GetThrottle();
+					}
+					
+					break;
+			}
+			
+			return 0;
 		}
 		
 		void Autonomous(void) {
@@ -150,56 +302,34 @@ class Robot2010Overkill : public SimpleRobot {
 				dsLCD->Printf(DriverStationLCD::kUser_Line2, 1, "                     ");
 				dsLCD->Printf(DriverStationLCD::kUser_Line3, 1, "                     ");
 				dsLCD->Printf(DriverStationLCD::kUser_Line4, 1, "                     ");
+				dsLCD->Printf(DriverStationLCD::kUser_Line5, 1, "                     ");
 				
 				dsLCD->Printf(DriverStationLCD::kUser_Line1, 1, "Kicker In");
 				dsLCD->Printf(DriverStationLCD::kUser_Line2, 1, "Pokey In");
 				dsLCD->Printf(DriverStationLCD::kUser_Line3, 1, "High Gear");
-				dsLCD->Printf(DriverStationLCD::kUser_Line4, 1, "Sucking Power:");
+				dsLCD->Printf(DriverStationLCD::kUser_Line4, 1, "Magnet Power:");
+				dsLCD->Printf(DriverStationLCD::kUser_Line5, 1, "Joystick Mode");
 				dsLCD->UpdateLCD();
 			
 			while (IsOperatorControl() && IsEnabled()) {
 				GetWatchdog().Feed(); // Feed the watchdog; keep our overlords happy.
 				compressorPump->Start();
-								
-				/* Drive Control */
-					/* Shifting */
-						if(joystickDriveLeft.GetRawButton(DRIVER_SHIFT_BUTTON)) {// || joystickDriveRight.GetRawButton(DRIVER_SHIFT_BUTTON)) {
-							solenoidShifter.Set(SOLENOID_SHIFTER_HIGH_POWER_DIRECTION);
-							
-							dsLCD->Printf(DriverStationLCD::kUser_Line3, 1, "                ");
-							dsLCD->Printf(DriverStationLCD::kUser_Line3, 1, "High Gear");
-							dsLCD->UpdateLCD();
-						} else {
-							solenoidShifter.Set(SOLENOID_SHIFTER_LOW_POWER_DIRECTION);
-							
-							dsLCD->Printf(DriverStationLCD::kUser_Line3, 1, "                ");
-							dsLCD->Printf(DriverStationLCD::kUser_Line3, 1, "Low Gear");
-							dsLCD->UpdateLCD();
-						}
-					
-					/* Drive Train */
-						driveTrain.TankDrive(joystickDriveLeft, joystickDriveRight);
-					
-					/* Pokey Control */
-						if(joystickDriveLeft.GetRawButton(DRIVER_POKEY_BUTTON) || joystickDriveRight.GetRawButton(DRIVER_POKEY_BUTTON)) {
-							solenoidPokey.Set(SOLENOID_POKEY_OUT_DIRECTION);
-							
-							dsLCD->Printf(DriverStationLCD::kUser_Line2, 1, "                ");
-							dsLCD->Printf(DriverStationLCD::kUser_Line2, 1, "Pokey Out");
-							dsLCD->UpdateLCD();
-						} else {
-							solenoidPokey.Set(SOLENOID_POKEY_IN_DIRECTION);
-							
-							dsLCD->Printf(DriverStationLCD::kUser_Line2, 1, "                ");
-							dsLCD->Printf(DriverStationLCD::kUser_Line2, 1, "Pokey In");
-							dsLCD->UpdateLCD();
-						}
+				
+				if(joystickDriveLeft.GetThrottle() == 0) {
+					dsLCD->Printf(DriverStationLCD::kUser_Line5, 1, "                     ");
+					dsLCD->Printf(DriverStationLCD::kUser_Line5, 1, "Xbox Mode");
+					dsLCD->UpdateLCD();
+				} else {
+					dsLCD->Printf(DriverStationLCD::kUser_Line5, 1, "                     ");
+					dsLCD->Printf(DriverStationLCD::kUser_Line5, 1, "Joystick Mode");
+					dsLCD->UpdateLCD();
+				}
 				
 				/* Manipulator Control */
 					/* Kicker Control */
 						switch(kickState) {
 							case 0:
-								if(joystickManipulator.GetRawButton(MANIPULATOR_KICK_BUTTON)) {
+								if(GetAction(kKick)) {
 									kickState = 1;
 									
 									kickTimer->Reset();
@@ -263,27 +393,76 @@ class Robot2010Overkill : public SimpleRobot {
 									dsLCD->Printf(DriverStationLCD::kUser_Line1, 1, "                     ");
 									dsLCD->Printf(DriverStationLCD::kUser_Line1, 1, "Kicker In");
 									dsLCD->UpdateLCD();
+								} else {
+									dsLCD->Printf(DriverStationLCD::kUser_Line1, 1, "                     ");
+									dsLCD->Printf(DriverStationLCD::kUser_Line1, 1, "Cooldown");
+									dsLCD->UpdateLCD();
 								}
+								
+								break;
 						}
 						
 						/* Ball Magnet Control */
-							floatThrottle = (joystickManipulator.GetThrottle() - 1) / 2;
+							floatThrottle = (GetAxis(kThrottle) - 1) / 2;
 							
 							dsLCD->Printf(DriverStationLCD::kUser_Line4, 1, "                 ");
 							dsLCD->Printf(DriverStationLCD::kUser_Line4, 1, "Magnet Power: %f", floatThrottle * -1);
 							
 							dsLCD->UpdateLCD();
 							
-							if(joystickManipulator.GetRawButton(MANIPULATOR_BALL_MAGNET_IN_BUTTON)) {
+							if(GetAction(kSuck)) {
 								motorBallMagnet1.Set(floatThrottle);
 								motorBallMagnet2.Set(floatThrottle);
-							} else if(joystickManipulator.GetRawButton(MANIPULATOR_BALL_MAGNET_OUT_BUTTON)) {
+							} else if(GetAction(kSpit)) {
 								motorBallMagnet1.Set(floatThrottle * -1);
 								motorBallMagnet2.Set(floatThrottle * -1);
 							} else {
 								motorBallMagnet1.Set(0.0);
 								motorBallMagnet2.Set(0.0);
 							}
+				
+				/* Drive Control */
+					/* Shifting */
+						if(GetAction(kShift)) {
+							solenoidShifter.Set(SOLENOID_SHIFTER_HIGH_POWER_DIRECTION);
+							
+							dsLCD->Printf(DriverStationLCD::kUser_Line3, 1, "                ");
+							dsLCD->Printf(DriverStationLCD::kUser_Line3, 1, "High Gear");
+							dsLCD->UpdateLCD();
+						} else {
+							solenoidShifter.Set(SOLENOID_SHIFTER_LOW_POWER_DIRECTION);
+							
+							dsLCD->Printf(DriverStationLCD::kUser_Line3, 1, "                ");
+							dsLCD->Printf(DriverStationLCD::kUser_Line3, 1, "Low Gear");
+							dsLCD->UpdateLCD();
+						}
+					
+					/* Drive Train */
+						driveTrain.TankDrive(GetAxis(kLeftY), GetAxis(kRightY));
+					
+					/* Pokey Control */
+						if(GetAction(kPokey)) {
+							solenoidPokey.Set(SOLENOID_POKEY_OUT_DIRECTION);
+							
+							if(kickState == 0) {
+								kickState = 4;
+								
+								kickTimer->Reset();
+								kickTimer->Start();
+							} else if(kickState == 4) {
+								kickTimer->Reset();
+							}
+							
+							dsLCD->Printf(DriverStationLCD::kUser_Line2, 1, "                ");
+							dsLCD->Printf(DriverStationLCD::kUser_Line2, 1, "Pokey Out");
+							dsLCD->UpdateLCD();
+						} else {
+							solenoidPokey.Set(SOLENOID_POKEY_IN_DIRECTION);
+							
+							dsLCD->Printf(DriverStationLCD::kUser_Line2, 1, "                ");
+							dsLCD->Printf(DriverStationLCD::kUser_Line2, 1, "Pokey In");
+							dsLCD->UpdateLCD();
+						}
 				
 				GetWatchdog().SetEnabled(false);
 				driveTrain.SetSafetyEnabled(false);
